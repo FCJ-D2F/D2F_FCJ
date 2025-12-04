ba994aa3985808b7e7ed0c3276b59946cc1dd7a6
hzanhle <130614439+hzanhle@users.noreply.github.com>
2025-12-05 02:16:52 +0700

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { vitePluginExpress } from './vite-plugin-express'
-import { createServer } from './server/index'
 
-export default defineConfig({
-  server: { host: '::' },
-  plugins: [
-    react(),
-    vitePluginExpress(createServer),
-  ],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      host: '::'
+    },
+
+    plugins: [
+      react(),
+    ],
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+
+    define: {
+      'process.env': env
+    }
+  }
 })
